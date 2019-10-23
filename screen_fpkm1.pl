cb822434d1d2d3f085de8d85bee217897dec3785
Guanqiao Feng <42392742+guanqiaofeng@users.noreply.github.com>
2019-10-23 11:04:40 -0500

    Update screen_fpkm1.pl

--- screen_fpkm1.pl
+++ screen_fpkm1.pl
@@ -1,5 +1,16 @@
 #!/usr/bin/perl
 
+# Input file format "ALL_fpkm1_screened.txt" (It contains all isoform which has fpkm > 1 in at least one sample)
+# XLOC_000001
+# XLOC_000004
+# XLOC_000006
+# XLOC_000015
+# XLOC_000024
+# XLOC_000030
+# XLOC_000033
+# XLOC_000047
+# XLOC_000054
+
 %hash = ();
 
 open FH, "<ALL_fpkm1_screened.txt";
